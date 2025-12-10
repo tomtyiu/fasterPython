32201c705a7b00710424a19e78ee2c5facb8fadb
Thomas YIu <tom.tyiu@gmail.com>
2025-12-10 10:12:54 -0800

    Update compute_squares.py

--- functions/compute_squares.py
+++ functions/compute_squares.py
@@ -1,3 +1,6 @@
+#Avoid Repeated Computations
+#Store results instead of recalculating.
+
 def compute_squares(nums):
     append = list.append  # local reference to speed up method lookup
     result = []
