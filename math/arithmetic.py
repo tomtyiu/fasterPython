98e6aee0a4b0fccf51ca3e37bf01ac8dbf41ab93
Thomas YIu <tom.tyiu@gmail.com>
2025-12-13 21:59:13 -0800

    Create arithmetic.py

---
+++ math/arithmetic.py
@@ -0,0 +1,55 @@
+The math module extends the list of mathematical functions available in Python. To use it, you must import the module:
+
+import math
+Common Functions in the Math Module
+
+math.sqrt(x): Returns the square root of a number.
+
+from math import sqrt
+def fast_sqrt(x):
+    return sqrt(x)
+
+from math import ceil
+def fast_ceil(x):
+    return ceil(x)
+
+from math import floor
+def fast_floor(x):
+    return floor(x)
+
+from math import pi
+def fast_pi()
+    return pi
+
+from math import factorial
+def fast_factorial:
+    return factorial(x)
+
+from math import log
+def fas_log(x,y)
+    return log(x, y)
+
+from math import exp
+def fast_exp(x):
+    return exp(x)
+
+from math import sin pi
+def fast_sin(x):
+    return sin(x)
+
+from math import e
+def fast_e:
+    return e
+
+from math import tau
+def fast_tau:
+    return tau
+
+from math import inf
+def fast_inf():
+    return inf
+
+
+from math import nan
+def fast_nan:
+    return nan
