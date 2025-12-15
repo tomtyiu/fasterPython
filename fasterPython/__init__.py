9590708acd9d626542b60e3da454bd078ed1cebf
Thomas YIu <tom.tyiu@gmail.com>
2025-12-15 10:34:20 -0800

    Update __init__.py

--- fasterPython/__init__.py
+++ fasterPython/__init__.py
@@ -1,19 +1,19 @@
 # Export functions from the basic sub-package
-from .fastPython import fast_sqrt
-from .fastPython import fast_ceil
-from .fastPython import fast_floor
-from .fastPython import fast_pi
-from .fastPython import fast_factorial
-from .fastPython import fas_log
-from .fastPython import fast_exp
-from .fastPython import fast_sin
-from .fastPython import fast_e
-from .fastPython import fast_tau
-from .fastPython import fast_inf
-from .fastPython import fast_nan
-from .fastPython import fib
-from .fastPython import sum_fast
-from .fastPython import compute_squares
-from .fastPython import sum_numpy
-from .fastPython import openai_api
+from .fasterPython import fast_sqrt
+from .fasterPython import fast_ceil
+from .fasterPython import fast_floor
+from .fasterPython import fast_pi
+from .fasterPython import fast_factorial
+from .fasterPython import fas_log
+from .fasterPython import fast_exp
+from .fasterPython import fast_sin
+from .fasterPython import fast_e
+from .fasterPython import fast_tau
+from .fasterPython import fast_inf
+from .fasterPython import fast_nan
+from .fasterPython import fib
+from .fasterPython import sum_fast
+from .fasterPython import compute_squares
+from .fasterPython import sum_numpy
+
 
