e54b35a8e24116ac30288b9b56890c61cab29f29
Thomas YIu <tom.tyiu@gmail.com>
2025-12-15 10:34:35 -0800

    Update fasterPython.py

--- fasterPython/fasterPython.py
+++ fasterPython/fasterPython.py
@@ -80,23 +80,3 @@ def sum_numpy(nums):
     arr = array(nums)
     return sum(arr)
 
-import os
-from openai import OpenAI
-
-
-def openai_api(prompt):
-    client = OpenAI(
-        # This is the default and can be omitted
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    )
-
-    create = client.responses.create
-    responses = create(
-        model="gpt-5.2",
-        instructions="You are a coding assistant that talks like a pirate.",
-        input=prompt,
-    )
-
-#print out response
-# responses=response.output_text
-# print(responses)
