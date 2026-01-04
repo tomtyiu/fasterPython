7cdf0404612542d9c1054b49f2a2b673d8675830
Thomas YIu <tom.tyiu@gmail.com>
2026-01-03 16:31:04 -0800

    Update fast-OpenAI-API.py

--- AI/OpenAI/fast-OpenAI-API.py
+++ AI/OpenAI/fast-OpenAI-API.py
@@ -7,7 +7,7 @@ client = OpenAI(
 )
 
 def OpenAIAPI(prompt):
-    #reduce or dot notation for speed up
+    #reduce or avoid multiple dot notation for speed up
     create = client.responses.create
     responses = create(
         model="gpt-5.2",
