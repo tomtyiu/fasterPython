e4b84634dcc318b0ffbd0370dfb4548092787af2
Thomas YIu <tom.tyiu@gmail.com>
2026-01-03 22:39:33 -0800

    Update fast_Bubble_Sort.py

--- functions/fast_Bubble_Sort.py
+++ functions/fast_Bubble_Sort.py
@@ -2,14 +2,12 @@
 mylist = [64, 34, 25, 12, 22, 11, 90, 5]
 
 n = len(mylist)
+# cache the list length in a local variable
 for i in range(n - 1):
-    swapped = False  # track if any swaps occurred
+    # reduce the range as the largest elements bubble to the end
     for j in range(n - i - 1):
-        a, b = mylist[j], mylist[j + 1]
+        a, b = mylist[j], mylist[j + 1]  # local variable assignment
         if a > b:
             mylist[j], mylist[j + 1] = b, a
-            swapped = True
-    if not swapped:
-        break  # list is sorted early
 
 print(mylist)
