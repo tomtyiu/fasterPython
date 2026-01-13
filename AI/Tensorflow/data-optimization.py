02c090af6c4527ec0b5eace082d6f6a547e44711
Thomas YIu <tom.tyiu@gmail.com>
2026-01-13 13:11:57 -0800

    Update data-optimization.py

--- AI/Tensorflow/data-optimization.py
+++ AI/Tensorflow/data-optimization.py
@@ -1,3 +1,7 @@
+import tensorflow as tf
+
+data = [1, 2, 3, 4, 5]
+
 dataset = tf.data.Dataset.from_tensor_slices(data)
 dataset = dataset.cache()                     # Cache data in memory
 dataset = dataset.shuffle(buffer_size=1000)   # Shuffle with an appropriate buffer
